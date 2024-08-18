a6149842ddfc93e847f783113789e5ededf578c2
Siddhartha-Hati <134580730+Siddhartha-Hati@users.noreply.github.com>
2024-08-18 19:21:32 +0530

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",
-    "corsheaders.middleware.CorsMiddleware"
+    "corsheaders.middleware.CorsMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
